c13eda8d9d6f459dbce19b9f0144d0dc1dba12fb
Dara <62209928+Moose-github@users.noreply.github.com>
2026-08-14 21:18:57 +0100

    Update BookStrategy.tsx

--- dashboard/src/views/BookStrategy.tsx
+++ dashboard/src/views/BookStrategy.tsx
@@ -1029,10 +1029,10 @@ function ConclusionsView() {
           </p>
           <ul>
             <li>
-              <strong>3.2b</strong> when using an <strong>8.0-dot specialist gym</strong>.
+              <strong>3.4b</strong> when using an <strong>8.0-dot specialist gym</strong>.
             </li>
             <li>
-              <strong>2.4b</strong> when using <strong>George&apos;s 7.3-dot gym</strong>.
+              <strong>2.9b</strong> when using <strong>George&apos;s 7.3-dot gym</strong>.
             </li>
           </ul>
           <p>
